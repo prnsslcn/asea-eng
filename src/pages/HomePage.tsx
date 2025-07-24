f1065b4a8778149c5e0fced5b08c252e84cc4e6e
Jungu Lee <prnsslcn@gmail.com>
2025-07-24 17:44:43 +0900

    remove: 불필요한 섹션 제거 및 메인 페이지 구조 최적화
    - ProgramsSection.tsx 삭제
    - StatsSection.tsx 삭제
    - HomePage.tsx에서 해당 섹션들 import 및 사용 제거

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import HeroSection from '../components/sections/HeroSection';
-// import ProgramsSection from '../components/sections/ProgramsSection';
-// import StatsSection from '../components/sections/StatsSection';
 import ContactSection from '../components/sections/ContactSection';
 import DepartmentsSection from "../components/sections/DepartmentsSection.tsx";
 import EmploymentShowcaseSection from "../components/sections/EmploymentShowcaseSection.tsx";
@@ -11,10 +9,8 @@ const HomePage: React.FC = () => {
     return (
         <>
             <HeroSection />
-            {/*<ProgramsSection />*/}
             <DepartmentsSection />
             <EmploymentShowcaseSection />
-            {/*<StatsSection />*/}
             <PartnershipShowcaseSection />
             <ContactSection />
         </>
